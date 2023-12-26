15cefb39fc2fa3a3dddcce6829a3302e420402fe
vladyatsuk <yatsuk.vladislav@gmail.com>
2023-12-26 22:46:59 +0200

    Add main file and config ofr render.com

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,5 +1,32 @@
 from flask import Flask
 
-app = Flask(__name__)
+from flask_migrate import Migrate
 
-import my_app.views
+
+from .views import healthcheck_blueprint, user_blueprint, category_blueprint, record_blueprint, currency_blueprint
+
+from my_app.db import db
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_pyfile('config.py', silent=True)
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:3228@localhost:5432/mydb'
+
+    # db = SQLAlchemy(app)
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    # app.app_context().push()
+
+    with app.app_context():
+        db.create_all()
+
+    app.register_blueprint(healthcheck_blueprint)
+    app.register_blueprint(user_blueprint)
+    app.register_blueprint(category_blueprint)
+    app.register_blueprint(record_blueprint)
+    app.register_blueprint(currency_blueprint)
+
+    return app
